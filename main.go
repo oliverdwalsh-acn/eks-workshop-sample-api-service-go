b2eb683b492fd7174199d48586a02c0fcbbbaba6
oliverdwalsh-acn <86722631+oliverdwalsh-acn@users.noreply.github.com>
2021-06-30 17:27:37 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World - Brand New Build"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
